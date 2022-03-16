a48688dd37ab4c2025b203c9c3f6aa5f366ffa87
Vivek Suthar <82870307+VIVEK-SUTHAR@users.noreply.github.com>
2022-03-16 14:35:58 +0530

    SKills Icon Added

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -4,6 +4,14 @@ import edu from "../Assets/edu.svg"
 import c from "../Assets/c.svg";
 import cpp from "../Assets/cpp.svg";
 import js from "../Assets/js.svg";
+import java from "../Assets/java.gif";
+import git from "../Assets/git.svg";
+import react from "../Assets/react.gif";
+import jq from "../Assets/jQuery.png";
+import linux from "../Assets/linux.png";
+import mysql from "../Assets/mysql.svg";
+import vscode from "../Assets/vscode.svg";
+import windows from "../Assets/win10.svg";
 export default function Resume() {
     return (
         <>
@@ -43,6 +51,7 @@ export default function Resume() {
                             <td>C++</td>
                         </tr>
                         <tr>
+                        <td><img src={java}></img></td>
                             <td>Java</td>
                         </tr>
                         <tr>
@@ -50,45 +59,51 @@ export default function Resume() {
                             <td>Javascript</td>
                         </tr>
                         <tr>
+                        <td><img src={cpp}></img></td>
                             <td>PHP</td>
                         </tr>
                     </table>
                     <table>
                         <th>Tools I use</th>
                         <tr>
-                            <td><img src={c}></img></td>
+                            <td><img src={vscode}></img></td>
                             <td>VS Code</td>
                         </tr>
                         <tr>
+                        <td><img src={git}></img></td>
                             <td>Git</td>
                         </tr>
                         <tr>
+                        <td><img src={linux}></img></td>
                             <td>Linux(WSL)</td>
                         </tr>
                         <tr>
+                        <td><img src={windows}></img></td>
                             <td>Windows</td>
                         </tr>
                         <tr>
+                        <td><img src={mysql}></img></td>
                             <td>MYSQL</td>
                         </tr>
                     </table>
                     <table>
                         <th>Libraries And FrameWorks I Use</th>
                         <tr>
-                            <td><img src={c}></img></td>
-                            <td>C</td>
+                            <td><img src={react}></img></td>
+                            <td>ReactJS</td>
                         </tr>
                         <tr>
-                            <td>C++</td>
+                        <td><img src={jq}></img></td>
+                            <td>jQuey</td>
                         </tr>
                         <tr>
-                            <td>Java</td>
+                            <td>NexrJS</td>
                         </tr>
                         <tr>
-                            <td>Javascript</td>
+                            <td>Bootstarp</td>
                         </tr>
                         <tr>
-                            <td>Php</td>
+                            <td>ChakraUI</td>
                         </tr>
                     </table>
                 </div>
