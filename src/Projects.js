f0b4b36c32f9a52316abe0f8bddc5371f4cffa37
Vivek Suthar <82870307+VIVEK-SUTHAR@users.noreply.github.com>
2022-04-03 13:20:35 +0530

    Project card added

--- src/Projects.js
+++ src/Projects.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import './projects.css'
+import msg from "./Assets/msg-clone.jpg"
 function Projects() {
     return (
         <div className='projectContainer'>
             <div className="projectHeading">What I have built</div>
             <div className="projectCards">
-                <div className="projectCard"></div>
+                <div className="projectCard">
+                    <h1>Messenger-clone</h1>
+                    <img src={msg} />
+                    <p>
+                        Tech Stack:<br></br>ReactJS
+                    </p>
+                </div>
                 <div className="projectCard"></div>
                 <div className="projectCard"></div>
                 <div className="projectCard"></div>
             </div>
-
         </div>
     )
 }
