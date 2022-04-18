9724b1ebc85d883bb23c7c49d60aa6f3cfbf4f19
Vivek Suthar <82870307+VIVEK-SUTHAR@users.noreply.github.com>
2022-04-18 18:18:39 +0530

    Contact Added

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,13 +5,13 @@ function Contact() {
         <div id='contactPage'>
             <h1>Contact Me</h1>
             <div className="contactForm">
-                <form name="contact" method="POST" data-netlify="true" action="/pages/success">
+                <form name="contact" method="POST" data-netlify="true" action="https://formspree.io/f/mqknynko">
                     <label htmlFor="Name">Your Name</label><br />
                     <input type="text" name="Name" id="Name" placeholder='Your Name' /><br />
                     <label htmlFor="Name">Your E-Mail ID</label><br />
                     <input type="email" name="mail" id="mail" placeholder='Your E-Mail Address' /><br />
                     <label htmlFor="Name">Your Message</label><br />
-                    <textarea></textarea><br/>
+                    <textarea></textarea><br />
                     <button type='submit' id='sendMSg'>Send Message</button>
                 </form>
             </div>
