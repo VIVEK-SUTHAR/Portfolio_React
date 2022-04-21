00317a224753c4bb50d3b88d20ea0fd5e53b324f
Vivek Suthar <82870307+VIVEK-SUTHAR@users.noreply.github.com>
2022-04-21 14:26:17 +0530

    fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import "./Navbar.css"
 import { AiOutlineHome, AiOutlineUser, AiOutlineBook, AiOutlineProject, AiOutlineContacts } from 'react-icons/ai';
 function Navbar() {
