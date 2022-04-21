37b42446616dba692b09ff8f1f579fd85ccc01f0
Vivek Suthar <82870307+VIVEK-SUTHAR@users.noreply.github.com>
2022-04-21 16:50:34 +0530

    Contact Backend added

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,18 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Contact.css"
+import db from './firebase'
 function Contact() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
+    const [issent, setIssent] = useState(false);
+    const sendMsg = (event) => {
+        event.preventDefault();
+        setName('');
+        setEmail('');
+        setMessage('');
+        db.collection('FORM_SUBMISSIONS').add({
+            name: name,
+            email: email,
+            message: message
+        });
+        setIssent(true);
+        setTimeout(() => {
+            setIssent(false);
+        }, 5000);
+    }
     return (
         <div id='contactPage'>
             <h1>Contact Me</h1>
+            {
+                issent && (
+                    <h3>Thank you {name} for your Message.</h3>
+                )
+            }
             <div className="contactForm">
-                <form name="contact" method="POST" data-netlify="true" action="https://formspree.io/f/mqknynko">
+                <form name="contact">
                     <label htmlFor="Name">Your Name</label><br />
-                    <input type="text" name="Name" id="Name" placeholder='Your Name' /><br />
+                    <input type="text" name="Name" id="Name" placeholder='Your Name' value={name} onChange={(e) => setName(e.target.value)} /><br />
                     <label htmlFor="Name">Your E-Mail ID</label><br />
-                    <input type="email" name="mail" id="mail" placeholder='Your E-Mail Address' /><br />
+                    <input type="email" name="mail" id="mail" placeholder='Your E-Mail Address'
+
+                        value={email} onChange={(e) => setEmail(e.target.value)}
+                    /><br />
                     <label htmlFor="Name">Your Message</label><br />
-                    <textarea></textarea><br />
-                    <button type='submit' id='sendMSg'>Send Message</button>
+                    <textarea
+                        value={message} onChange={(e) => setMessage(e.target.value)}
+                    ></textarea><br />
+                    <button type='submit' id='sendMSg' onClick={sendMsg}>Send Message</button>
                 </form>
             </div>
         </div>
