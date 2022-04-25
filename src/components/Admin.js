7e80b37818905095452d72f3ef954eef7a949b08
Vivek Suthar <82870307+VIVEK-SUTHAR@users.noreply.github.com>
2022-04-25 19:01:18 +0530

    code formatted

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -9,11 +9,11 @@ function Admin() {
     const [isWrongCrenditials, setIsWrongCrenditials] = useState(false)
     useEffect(() => {
         db
-        .collection('FORM_SUBMISSIONS')
-        .orderBy("timestamp","desc")
-        .onSnapshot((snapshot) => {
-            setMessages(snapshot.docs.map(doc => ({ id: doc.id, data: doc.data() })))
-        })
+            .collection('FORM_SUBMISSIONS')
+            .orderBy("timestamp", "desc")
+            .onSnapshot((snapshot) => {
+                setMessages(snapshot.docs.map(doc => ({ id: doc.id, data: doc.data() })))
+            })
     }, []);
     useEffect(() => {
         auth.onAuthStateChanged((user) => {
@@ -41,46 +41,43 @@ function Admin() {
     return (
         <>
             {
-                !loggedin && (<>
-                    <div className='flex'>
-                        <form>
-                            {isWrongCrenditials && (
-                                <h2 style={{ color: "red" }}>The email id or password is wrong</h2>
-                            )}
-                            <label htmlFor="email">Emial Id</label>
-                            <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
-                            <label htmlFor="password">Passsword</label>
-                            <input type="password" value={passworsd} onChange={(e) => setPassworsd(e.target.value)} />
-                            <button type='submit' onClick={validateUser}>
-                                Login
-                            </button>
-                        </form>
-                    </div>
-                </>
+                !loggedin ? (
+                    <>
+                        <div className='flex'>
+                            <form>
+                                {!!isWrongCrenditials && (
+                                    <h2 style={{ color: "red" }}>The email id or password is wrong</h2>
+                                )}
+                                <label htmlFor="email">Emial Id</label>
+                                <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+                                <label htmlFor="password">Passsword</label>
+                                <input type="password" value={passworsd} onChange={(e) => setPassworsd(e.target.value)} />
+                                <button type='submit' onClick={validateUser}>
+                                    Login
+                                </button>
+                            </form>
+                        </div>
+                    </>
+                ) : (
+                    <>
+                        <button className='logout' onClick={logout}>Log out</button>
+                        <div className='AContainer'>
+                            {
+                                messages && messages.map((el) => (
+                                    <div key={el.id} className='message-card'>
+                                        Name:{el.data.name}
+                                        <br />
+                                        email:{el.data.email}
+                                        <br />
+                                        Message:{el.data.message}
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    </>
                 )
             }
 
-
-
-            {loggedin && (
-                <>
-                    <button className='logout' onClick={logout}>Log out</button>
-
-                    <div className='AContainer'>
-                        {
-                            messages && messages.map((el) => (
-                                <div key={el.id} className='message-card'>
-                                    Name:{el.data.name}
-                                    <br />
-                                    email:{el.data.email}
-                                    <br />
-                                    Message:{el.data.message}
-                                </div>
-                            ))
-                        }
-                    </div>
-                </>)
-            }
         </>
     )
 }
