fdbdcf7c375ce5ac7815783fd07600baee091fbf
Vivek Suthar <82870307+VIVEK-SUTHAR@users.noreply.github.com>
2022-05-27 15:28:14 +0530

    Projects Animation Addeed

--- src/Projects.js
+++ src/Projects.js
@@ -20,9 +20,9 @@ function Projects() {
                                 <motion.div
                                     initial={{
                                         opacity: 0,
-                                        x: -300
+                                        y: 200
                                     }}
-                                    whileInView={{ opacity: 1, x: 0 }}
+                                    whileInView={{ opacity: 1, y: 0 }}
                                     transition={{ duration: 1 }}
                                     viewport={{ once: false }}
                                 >
