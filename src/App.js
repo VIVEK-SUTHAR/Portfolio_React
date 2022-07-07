31bb3167c40322793203f8333519599b4bc10d25
Vivek Suthar <82870307+VIVEK-SUTHAR@users.noreply.github.com>
2022-07-07 09:12:00 +0530

    Loader Added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
+import { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Admin from "./components/Admin";
 import Contact from "./components/Contact";
+import Loader from "./components/Loader";
 import Main from "./components/Main";
 import Me from "./components/Me";
 import Navbar from "./components/Navbar";
 import Projects from "./Projects";
 function App() {
-
-
+  const [isLoading, setIsLoading] = useState(true);
+  window.addEventListener('load', () => {
+    setIsLoading(false);
+  })
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/admin" element={<Admin />} />
-        <Route path="/about" element={<Me />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </Router>
+    isLoading ? (
+      <Loader />
+
+    ) :
+      (
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/admin" element={<Admin />} />
+            <Route path="/about" element={<Me />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </Router>
+      )
   );
 }
 
---
+++ src/components/Loader.js
@@ -0,0 +1,11 @@
+import "./Loader.css"
+function Loader() {
+    return (
+        <div className="loaderContainer">
+            <div className="loader"></div>
+            <div className="text">Good things take Time</div>
+        </div>
+    )
+}
+
+export default Loader
