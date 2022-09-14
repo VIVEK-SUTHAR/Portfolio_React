dd1f07531be0f0cf0e43b5f3046519f669370505
VIVEK-SUTHAR <sutharvivek18@gmail.com>
2022-09-14 08:06:30 +0530

    Typo:fixed in page

--- src/components/Achivements.js
+++ src/components/Achivements.js
@@ -2,34 +2,35 @@ import "./Achivements.css"
 import { motion } from "framer-motion"
 export default function Achivements() {
     return (
-        // <section className="achivements_container">AC</section>
-        <>
-            <center className="title">Achivements</center>
-            <motion.div class="achivements_container"
-                initial={{ x: "-100%" }}
-                whileInView={{ scale: 1, x: 0 }}
-                transition={{ type: 'spring', duration: 1 }}
-                viewport={{ once: true }}>
-                <motion.span
-                    className='achivemets'
-                    initial={{ x: "-100%" }}
-                    whileInView={{ scale: 1, x: 0 }}
-                    transition={{ type: 'spring', duration: 1 }}
-                    viewport={{ once: true }}
-                >
-                    <ul class="sessions">
-                        <li className="li">
-                            <div class="time">Won Wild Hacks-II, A MLH Hackathon </div>
-                            <p>Built MetaMusic</p>
-                        </li>
-                        <li className="li">
-                            <div class="time">Won DUHacks,A Hackathon By GDSC</div>
-                            <p>Built DeBlog</p>
-                        </li>
-
-                    </ul>
-                </motion.span>
-            </motion.div>
-        </>
-    )
+      // <section className="achivements_container">AC</section>
+      <>
+        <center className="title">Achievements</center>
+        <motion.div
+          class="achivements_container"
+          initial={{ x: "-100%" }}
+          whileInView={{ scale: 1, x: 0 }}
+          transition={{ type: "spring", duration: 1 }}
+          viewport={{ once: true }}
+        >
+          <motion.span
+            className="achivemets"
+            initial={{ x: "-100%" }}
+            whileInView={{ scale: 1, x: 0 }}
+            transition={{ type: "spring", duration: 1 }}
+            viewport={{ once: true }}
+          >
+            <ul class="sessions">
+              <li className="li">
+                <div class="time">Won Wild Hacks-II, A MLH Hackathon </div>
+                <p>Built MetaMusic</p>
+              </li>
+              <li className="li">
+                <div class="time">Won DUHacks,A Hackathon By GDSC</div>
+                <p>Built DeBlog</p>
+              </li>
+            </ul>
+          </motion.span>
+        </motion.div>
+      </>
+    );
 }
