07cc0a99c59e8840e32d313be82524c50e79cf02
VIVEK <sutharvivek18@gmail.com>
2022-09-20 18:25:26 +0530

    fea: Updated

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,6 @@ import Skills from '../Skills';
 import About from "./About";
 import Achivements from "./Achivements";
 import Footer from "./Footer";
-import Tweets from "./Tweets"
 import Me from "./Me";
 import Resume from "./Resume";
 function Main() {
